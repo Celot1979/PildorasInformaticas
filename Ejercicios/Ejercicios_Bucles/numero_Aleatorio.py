de6af40baf77192daba4fe35db37497aae5fb730
DaniGilMar <90446291+DaniGilMar@users.noreply.github.com>
2023-01-06 10:06:18 +0100

    Mumero_Maximo_Intentos

--- Ejercicios/Ejercicios_Bucles/numero_Aleatorio.py
+++ Ejercicios/Ejercicios_Bucles/numero_Aleatorio.py
@@ -1,6 +1,6 @@
 import random
-numero_aleatorio = random.randint(0,100)
-print (numero_aleatorio)
+numero_aleatorio = random.randint(0, 100)
+print(numero_aleatorio)
 cont = 0
 usuario = int(input(" Escribe un número \n"))
 acierto = True
@@ -9,17 +9,21 @@ while acierto == True:
     if numero_aleatorio == usuario:
         cont += 1
         if cont == 1:
-             print ("Correcto. Has utilizado… " + str(cont) + " sólo intento ")
+            print("Correcto. Has utilizado… " + str(cont) + " sólo intento ")
         else:
-             print ("Correcto. Has utilizado… " + str(cont) + " nº de intentos consumidos ")
+            print("Correcto. Has utilizado… " +
+                  str(cont) + " nº de intentos consumidos ")
         acierto = False
     else:
-        if(numero_aleatorio < usuario):
-            print("El número introduccido es mayor que el aleatorio ")
-            cont += 1
-            usuario = int(input(" Escribe otro número \n"))
-        elif (numero_aleatorio > usuario):
-            print("El número introduccido es menor que el aleatorio ")
-            cont +=1
-            usuario = int(input(" Escribe otro número \n"))
-    
+        if cont < 3:
+            if(numero_aleatorio < usuario):
+                print("El número introduccido es mayor que el aleatorio ")
+                cont += 1
+                usuario = int(input(" Escribe otro número \n"))
+            elif (numero_aleatorio > usuario):
+                print("El número introduccido es menor que el aleatorio ")
+                cont += 1
+                usuario = int(input(" Escribe otro número \n"))
+        elif (cont == 3):
+            print("Te has excedido del número de intentos")
+            acierto= False
